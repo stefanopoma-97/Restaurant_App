915643bdff2c06a311af21cfef8f8f4365c524d8
Stefano <stefano1997poma97@gmail.com>
2022-03-13 15:28:26 +0100

    layout per edit ristorante

---
+++ app/src/main/java/com/poma/restaurant/restaurant/Activity_Create_Restaurant.java
@@ -0,0 +1,14 @@
+package com.poma.restaurant.restaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Create_Restaurant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_restaurant);
+    }
+}
