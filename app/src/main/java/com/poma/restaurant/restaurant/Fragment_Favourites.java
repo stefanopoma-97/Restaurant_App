f9b54165dc8558704d62d0c5083d60a852e4914f
Stefano <stefano1997poma97@gmail.com>
2022-03-18 12:02:32 +0100

    search per preferiti

--- app/src/main/java/com/poma/restaurant/restaurant/Fragment_Favourites.java
+++ app/src/main/java/com/poma/restaurant/restaurant/Fragment_Favourites.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -39,6 +40,9 @@ import java.util.ArrayList;
 public class Fragment_Favourites extends Fragment {
 
     private static final String TAG_LOG = Fragment_Favourites.class.getName();
+    private static final String SEARCH_KEY_FRAGMENT_FAVOURITES_LIST = "com.poma.restaurant.SEARCH_KEY_FRAGMENT_FAVOURITES__LIST";
+
+
     private RecyclerView rv;
     private ArrayList<Favourite> mdata;
     private FirebaseAuth mAuth;
@@ -49,6 +53,9 @@ public class Fragment_Favourites extends Fragment {
     private SwipeRefreshLayout swipeRefreshLayout;
     private RecyclerViewAdapter_Favourite adapter;
     private SearchView searchView = null;
+
+    private TextView textView_no_result;
+    private String search = "";
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -101,6 +108,8 @@ public class Fragment_Favourites extends Fragment {
 
 
         this.searchView = view.findViewById(R.id.search_view_fragment_favourite);
+        this.textView_no_result = view.findViewById(R.id.textview_no_result_favourites_list);
+        this.textView_no_result.setVisibility(View.INVISIBLE);
 
         this.swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_refresh_fragment_favourite);
         this.swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -113,6 +122,34 @@ public class Fragment_Favourites extends Fragment {
 
 
         return view;
+    }
+
+    //State
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+
+        savedInstanceState.putString(SEARCH_KEY_FRAGMENT_FAVOURITES_LIST, this.searchView.getQuery().toString());
+        super.onSaveInstanceState(savedInstanceState);
+        Log.d(TAG_LOG,"Save state: "+SEARCH_KEY_FRAGMENT_FAVOURITES_LIST+" valore: "+this.searchView.getQuery().toString());
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d(TAG_LOG,"on Activity Create");
+
+        if (savedInstanceState != null){
+            this.search = savedInstanceState.getString(SEARCH_KEY_FRAGMENT_FAVOURITES_LIST);
+            //searchView.setQuery(search, true);
+
+
+            Log.d(TAG_LOG,"Retrive state: "+SEARCH_KEY_FRAGMENT_FAVOURITES_LIST+" valore: "+search);
+        }
+        else {
+            Log.d(TAG_LOG,"Retrive state: "+SEARCH_KEY_FRAGMENT_FAVOURITES_LIST+" valore: null");
+        }
+
     }
 
     @Override
@@ -159,6 +196,7 @@ public class Fragment_Favourites extends Fragment {
 
         rv.setLayoutManager(linearLayoutManager);
         rv.setAdapter(adapter);
+        this.textView_no_result.setVisibility(View.VISIBLE);
 
         if (this.currentUser!=null){
             Query query = this.db.collection("favourites").whereEqualTo("user_id",this.mAuth.getCurrentUser().getUid());
@@ -179,17 +217,36 @@ public class Fragment_Favourites extends Fragment {
                                 mdata.add(createFavourite(dc.getDocument()));
                                 setAdapterChange();
 
+                                if (searchView.getQuery().toString().equals(""))
+                                    Log.d(TAG_LOG,"Controllo search view, è vuota non faccio nulla");
+                                else {
+                                    Log.d(TAG_LOG, "Controllo search view, contine qualcosa, applico filtro");
+                                    filter(searchView.getQuery().toString());
+                                }
+
                                 break;
                             case MODIFIED:
                                 Log.d(TAG_LOG, "modify notify: " + dc.getDocument().getData());
                                 int pos = removeRestaurantFromData(dc.getDocument().getId());
                                 mdata.add(pos, createFavourite(dc.getDocument()));
                                 setAdapterChange();
+                                if (searchView.getQuery().toString().equals(""))
+                                    Log.d(TAG_LOG,"Controllo search view, è vuota non faccio nulla");
+                                else{
+                                    Log.d(TAG_LOG,"Controllo search view, contine qualcosa, applico filtro");
+                                    filter(searchView.getQuery().toString());
+                                }
                                 break;
                             case REMOVED:
                                 Log.d(TAG_LOG, "Removed notify (id = "+dc.getDocument().getId()+"): " + dc.getDocument().getData());
                                 removeRestaurantFromData(dc.getDocument().getId());
                                 setAdapterChange();
+                                if (searchView.getQuery().toString().equals(""))
+                                    Log.d(TAG_LOG,"Controllo search view, è vuota non faccio nulla");
+                                else{
+                                    Log.d(TAG_LOG,"Controllo search view, contine qualcosa, applico filtro");
+                                    filter(searchView.getQuery().toString());
+                                }
                                 break;
                         }
                     }
@@ -234,9 +291,7 @@ public class Fragment_Favourites extends Fragment {
 
     private void setSearchView(){
         //al cambio di orientazione la searchview viene resettata
-        searchView.setQuery("", false);
-        searchView.clearFocus();
-        searchView.setIconified(true);
+
         this.searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
@@ -264,11 +319,19 @@ public class Fragment_Favourites extends Fragment {
                 return true;
             }
         });
+
+        Log.d(TAG_LOG,"recupero valore da mettere in search");
+        if (this.search.equals(""))
+            searchView.clearFocus();
+        else{
+            Log.d(TAG_LOG,"Set query");
+            searchView.setQuery(this.search, true);
+        }
     }
 
     private void filter(String text) {
         Log.d(TAG_LOG,"filter");
-        this.listener_notification.remove();
+        //this.listener_notification.remove();
         ArrayList<Favourite> filteredList = new ArrayList<>();
 
 
@@ -297,6 +360,11 @@ public class Fragment_Favourites extends Fragment {
         Log.d(TAG_LOG,"set Adapter");
 
         this.adapter.notifyDataSetChanged();
+
+        if(adapter.getItemCount()==0)
+            this.textView_no_result.setVisibility(View.VISIBLE);
+        else
+            this.textView_no_result.setVisibility(View.INVISIBLE);
 
     }
 
